f2ae91afe90c2bcb96ced4cbfde68546a170bf86
James Lawrence <jamdlaw@yahoo.com>
2022-03-04 20:28:45 -0800

    removed bad code

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <Timer time_to_complete="30"/>
-  <Message inputMsg = 'test'/>
 </template>
 
 <script>
 import Timer from './components/Timer.vue'
-import Message from './components/Message.vue'
 
 export default {
   name: 'App',
   components: {
-    Timer,
-    Message
+    Timer
   }
 }
 </script>
