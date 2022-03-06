24a56505001000a790437ddb9c42254a046d5ccb
James Lawrence <jamdlaw@yahoo.com>
2022-03-06 12:01:54 -0800

    added a timer that counts down

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,75 +1,41 @@
 <template>
-  <div id="app">
-  <div class="container" style="max-width: 600px">
-    <div class="d-flex mt-5">
-      <input
-        type="text"
-        v-model="task"
-        placeholder="Enter task"
-        class="w-100 form-control"
-      />
-    <button class="btn btn-warning rounded-0 mr-1" @click="start">Start</button>
-    <button class="btn btn-warning rounded-0 mr-1" @click="stop">Stop</button>
-    <button class="btn btn-warning rounded-0 mr-1" @click="reset">Reset</button>
+  <div>
+    <div>
+      <span>{{timer.days}}</span>:<span>{{timer.hours}}</span>:<span>{{timer.minutes}}</span>:<span>{{timer.seconds}}</span>
     </div>
-
-    <p>{{formattedElapsedTime}}</p>
-  <table class="table table-bordered mt-5">
-    <thead>
-        <tr>
-          <th scope="col">Task</th>
-          <th scope="col" style="width: 120px">Duration</th>
-        </tr>
-      </thead>
-    <tr v-for="(task, index) in tasks" :key="index">
-      <td>{{ task.name }}</td> 
-      <td>{{task.duration}}</td>
-    </tr>
-    </table>
-    </div>
+    <p>{{timer.isRunning ? 'Running' : 'Not running'}}</p>
+    <button @click="timer.start()">Start</button>
+    <button @click="timer.pause()">Pause</button>
+    <button @click="timer.resume()">Resume</button>
+    <button @click="restartFive()">Restart</button>
   </div>
 </template>
-
 <script>
-import { nextTick } from 'vue'
-
-export default {
-  name: "App",
-  data() {
+import { defineComponent, watchEffect, onMounted } from "vue";
+import { useTimer } from 'vue-timer-hook';
+export default defineComponent({
+  name: "Home",
+  setup() {
+    const time = new Date();
+    time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
+    const timer = useTimer(time);
+    const restartFive = () => {
+        // Restarts to 5 minutes timer
+        const time = new Date();
+        time.setSeconds(time.getSeconds() + 300);
+        timer.restart(time);
+    }
+    onMounted(() => {
+      watchEffect(async () => {
+        if(timer.isExpired.value) {
+            console.warn('IsExpired')
+        }
+      })
+    })
     return {
-      task: "",
-      elapsedTime: 0,
-      timer: undefined,
-      tasks: []
-    };
+        timer,
+        restartFive,
+     };
   },
-  computed: {
-    formattedElapsedTime() {
-      const date = new Date(null);
-      date.setSeconds(this.elapsedTime / 1000);
-      const utc = date.toUTCString();
-      return utc.substr(utc.indexOf(":") - 2, 8);
-    }
-  },
-  methods: {
-   async start() {
-      this.timer = setInterval(() => {
-        this.elapsedTime += 1000;
-      }, 1000);
-       await nextTick()
-      // DOM is now updated
-      document.title = this.formattedElapsedTime;
-    },
-    stop() {
-      this.tasks.push({  name: this.task,
-                        duration: this.formattedElapsedTime
-             })
-      clearInterval(this.timer);
-    },
-    reset() {
-      this.elapsedTime = 0;
-    }
-  }
-
-};
+});
 </script>
